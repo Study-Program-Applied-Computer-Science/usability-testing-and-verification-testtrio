724e5f2d448b19f0242e3bdc3db866092e945494
Mohith Tummala <137559580+DevMohith@users.noreply.github.com>
2025-03-08 05:15:09 +0100

    Fixed Imports Issues

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Navbar from "./components/Navbar/Navbar.jsx";
 import Home from "./components/Home/Home.jsx";
 import Calender from "./components/Calender/Calender.jsx";
 import UserLogin from "./components/UserLogin/UserLogin.jsx";
-import Events from './components/Events/events.jsx';
+import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/eventDetails.jsx';
 import MyEvents from './components/Events/myEvents/myEvents.jsx';
 import AllEvents from './components/Events/allEvents/allEvents.jsx';
