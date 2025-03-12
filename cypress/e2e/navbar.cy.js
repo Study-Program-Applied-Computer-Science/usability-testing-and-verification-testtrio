8a8928ff62b01fdde102ea62cdb4ad898dcc14fd
Mohith Tummala <137559580+DevMohith@users.noreply.github.com>
2025-03-12 18:36:08 +0100

    written a addon test for clicking nested routes under Events parent route

--- cypress/e2e/navbar.cy.js
+++ cypress/e2e/navbar.cy.js
@@ -19,6 +19,10 @@ describe("Tests for Navbar Navigation",() => {
         cy.url().should("include", "/Events");
         cy.wait(2000);
 
+        // now look for All Events
+        cy.contains("button", "All Events").click();
+        cy.wait(3000);
+
         // now go back to home route again
         cy.get("nav").contains("Home").click();
         cy.url().should("eq", "http://localhost:5173/");
@@ -27,6 +31,5 @@ describe("Tests for Navbar Navigation",() => {
         //log0t now
         cy.get("button").contains("Logout").click();
         cy.url().should("include", "/UserLogin");
-
     });
 });
